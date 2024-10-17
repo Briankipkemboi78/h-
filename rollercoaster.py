885ce4b6aeca20bfeab4314c84b97ff8eeb17849
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-17 09:58:25 +0300

    Adding Photo requirement

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,15 +1,25 @@
 print("Welcome to rollercoaster!")
 height = int(input("Enter your Height in cm:\n"))
+bill = 0
 
 
 if height >= 120:
     print("You can Ride")
     age = int(input("Enter your age:\n"))
     if age <= 12:
-        print("Please pay $5 for the ride!")
+        bill = 5
+        print("Child tickets are $5!")
     elif age <= 18:
-        print("Please pay $7 for the ride")
+        bill = 7
+        print("Youth tickets are $7")
     else:
-        print("Please pay $12 for the ride")
+        bill = 12
+        print("Adult tickets are $12")
+
+    wants_photo = input("Do you want a photo? Type y for Yes and n for No.\n ")
+    if wants_photo == 'y':
+        bill += 3
+        print(f"Your total bill is ${bill}")
+
 else:
     print("Sorry you need to grow some hair.")
