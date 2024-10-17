b31db45228e164a50ce39a046c38be3fb1a8a093
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-17 08:24:29 +0300

    Refactored the code

--- rollercoaster.py
+++ rollercoaster.py
@@ -5,9 +5,9 @@ height = int(input("Enter your Height in cm:\n"))
 if height >= 120:
     print("You can Ride")
     age = int(input("Enter your age:\n"))
-    if age < 12:
+    if age <= 12:
         print("Please pay $5 for the ride!")
-    elif age >= 12 and age < 18:
+    elif age <= 18:
         print("Please pay $7 for the ride")
     else:
         print("Please pay $12 for the ride")
