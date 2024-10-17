cc2139f36f2b0c1735ceb5b2753570f87fb77b38
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-17 08:13:58 +0300

    Adding age condition

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,7 +1,11 @@
 print("Welcome to rollercoaster!")
 height = int(input("Enter your Height in cm:\n"))
+age = int(input("Enter your age:\n"))
 
-if height > 120:
-    print("Enjoy the ride!")
+if height >= 120:
+    if age > 18:
+        print("Please pay $12 for the ride!")
+    else:
+        print("Please pay $7 for the ride")
 else:
     print("Sorry you need to grow some hair.")
