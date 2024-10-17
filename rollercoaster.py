bbcd22a04d2d80db04a48a21619c7d160461d66d
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-17 08:19:13 +0300

    Adding more age band

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,11 +1,15 @@
 print("Welcome to rollercoaster!")
 height = int(input("Enter your Height in cm:\n"))
-age = int(input("Enter your age:\n"))
+
 
 if height >= 120:
-    if age > 18:
-        print("Please pay $12 for the ride!")
-    else:
+    print("You can Ride")
+    age = int(input("Enter your age:\n"))
+    if age < 12:
+        print("Please pay $5 for the ride!")
+    elif age >= 12 and age < 18:
         print("Please pay $7 for the ride")
+    else:
+        print("Please pay $12 for the ride")
 else:
     print("Sorry you need to grow some hair.")
