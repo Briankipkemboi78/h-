f786587838c6c9b7902a22fb603b43234c53c01f
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-17 07:45:34 +0300

    Initial commit

---
+++ rollercoaster.py
@@ -0,0 +1,7 @@
+print("Welcome to rollercoaster!")
+height = int(input("Enter your Height in cm:\n"))
+
+if height > 120:
+    print("Enjoy the ride!")
+else:
+    print("Sorry you need to grow some hair.")
